d3ff9f68df7a445048956b4f8046ce0c0ae69d85
louisberns <louisuntitled@gmail.com>
2018-04-12 17:58:18 -0300

    Added animation bottom bar

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 const menu = document.querySelector(".menu-wrap");
 const toggleMenuIcon = document.querySelector("#toggle-menu-icon");
+const footer = document.querySelector(".footer-block");
 
 toggleMenuIcon.addEventListener("click", () => {
   const toggleMenu = menu.classList.toggle("menu-wrap___closed");
   return (toggleMenu ? toggleMenuIcon.innerHTML = "menu" : toggleMenuIcon.innerHTML = "close");
 });
+
+footer.addEventListener("mouseenter", () => {
+  const toggleFooter = footer.classList.toggle("auto-toggle-footer");
+
+  if (toggleFooter) {
+    this.setTimeout(() => footer.classList.toggle("auto-toggle-footer"), 3000);
+  } else {
+    return;
+  }
+});
+
+function toggleBack(func) {
+
+}

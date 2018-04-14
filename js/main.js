baf1c8b77f3880118cdff88d1e50af01285eed53
louisberns <louisuntitled@gmail.com>
2018-04-13 22:37:20 -0300

    Added menu transition

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
-const menu = document.querySelector(".menu-wrap");
-const toggleMenuIcon = document.querySelector("#toggle-menu-icon");
+const menu = {
+  wrap: document.querySelector(".menu-wrap"),
+  content: [
+    document.querySelector(".menu-block__header"),
+    document.querySelector(".menu-block__info"),
+    document.querySelector(".menu-block__opt")
+  ],
+  patient: document.querySelector("#patient-pic")
+};
+const dock = document.querySelector(".dock-wrap");
+const toggleMenuIcon = document.getElementsByClassName("toggle-menu-icon");
+const iconMenu = document.querySelector("#toggle-menu-icon");
+const content = document.querySelector("#content");
 const footer = document.querySelector(".footer-block");
 
-toggleMenuIcon.addEventListener("click", () => {
-  const toggleMenu = menu.classList.toggle("menu-wrap___closed");
-  return (toggleMenu ? toggleMenuIcon.innerHTML = "menu" : toggleMenuIcon.innerHTML = "close");
+Array.from(toggleMenuIcon).filter(i => {
+  i.addEventListener("click", () => {
+    const toggleMenu = menu.wrap.classList.toggle("menu-wrap___closed");
+
+    return (toggleMenu ? iconMenu.innerHTML = "menu" : iconMenu.innerHTML = "close");
+  })
 });

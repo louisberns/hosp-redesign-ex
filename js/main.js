c724ca672f9840525e9c2ee52c27d7b84aceca3e
louisberns <louisuntitled@gmail.com>
2018-04-17 23:10:15 -0300

    Added mobile style

--- js/main.js
+++ js/main.js
@@ -10,16 +10,16 @@ const menu = {
 const dock = document.querySelector(".dock-wrap");
 const toggleMenuIcon = document.getElementsByClassName("toggle-menu-icon");
 const iconMenu = document.querySelector("#toggle-menu-icon");
+const menuWrapClass = "menu-wrap___closed";
 const content = document.querySelector("#content");
 const footer = document.querySelector(".footer-block");
-const menuWrapClass = "menu-wrap___closed";
+const loader = document.querySelector("#loader");
 
 function toggleMenu() {
   const toggleMenu = menu.wrap.classList.toggle(menuWrapClass);
 
   return (toggleMenu ? iconMenu.innerHTML = "menu" : iconMenu.innerHTML = "close");
 }
-
 
 Array.from(toggleMenuIcon).filter(i => {
   i.addEventListener("click", () => {
@@ -30,5 +30,17 @@ Array.from(toggleMenuIcon).filter(i => {
 window.addEventListener("resize", i => {
   if (i.target.innerWidth <= 1100 && !menu.wrap.classList.contains(menuWrapClass)) {
     toggleMenu();
-  }
+  } else { return }
 });
+
+document.onreadystatechange = function () {
+  if (document.readyState === "loading") {
+    loader.classList.add("active");
+  } else if (document.readyState === "interactive") {
+    if (window.innerWidth <= 1100 && !menu.wrap.classList.contains(menuWrapClass)) {
+      toggleMenu();
+    }
+  } else if (document.readyState === "complete") {
+    window.setTimeout(() => loader.classList.remove("active"), 1000);
+  }
+}
